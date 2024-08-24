ffe3d874252c414fb47324b7b4b7f7da28b1829e
Dns <senhordasestrelas1223@gmail.com>
2024-08-23 23:06:16 -0300

    #

---
+++ Dio-Aulas/dart/meu_app/bin/meu_app.dart
@@ -0,0 +1,30 @@
+//import 'package:meu_app/meu_app.dart' as meu_app;
+
+void main(List<String> arguments) {
+  int number1 = 10;
+  var number2 = 11;
+
+  print("Retorne verdadeiro se e somente se esse inteiro for par.");
+  print(number1.isEven);
+  print(number2.isEven);
+
+  print("Retorne verdadeiro se e somente se esse inteiro for impar.");
+  print(number1.isOdd);
+  print(number2.isOdd);
+
+  //print('Hello world: ${meu_app.calculate()}!');
+  // String text = "DIO";
+  // var text2 = "DIO";
+  // String text3;
+
+  // int number1 = 1;
+  // var number2 = 2;
+  // int number3;
+
+  // List<String> list = [];
+  // list.add("A");
+  // var list1 = [];
+  // list1.add("A");
+  // list1.add(1);
+  // list1.add(1.2);
+}
